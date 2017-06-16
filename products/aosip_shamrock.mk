345d669c17ec98e4e2b71e5d96d4c017dd5153d2
Burak "TheNexus" Dede <burak.dede.03@gmail.com>
2017-06-17 01:29:43 +0300

    Add shamrock to the time machine

---
+++ products/aosip_shamrock.mk
@@ -0,0 +1,45 @@
+#
+# Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Inherit from shamrock device
+$(call inherit-product, device/google/shamrock/device.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := google
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := shamrock
+PRODUCT_NAME := aosip_shamrock
+PRODUCT_MANUFACTURER := General Mobile
+PRODUCT_MODEL := GM 5 Plus
+TARGET_VENDOR := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+# Device maintainer
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Burak Dede (TheNexus)"
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=qcom/shamrock/shamrock:7.1.1/NNH26/3945561:user/dev-keys \
+    PRIVATE_BUILD_DESC="shamrock-user 7.1.1 NNH26 3945561 release-keys"
+endif
